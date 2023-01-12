50a30b5040a22a6c25dc812efb3669246483f0ab
Ziad Lteif <ziad.ltf@gmail.com>
2023-01-12 06:13:05 +0000

    Added prohibited users and messages context

--- music_cog.py
+++ music_cog.py
@@ -1,11 +1,22 @@
 from ast import alias
 import discord
 from discord.ext import commands
-
+import random
 from youtube_dl import YoutubeDL
 #import tasks
+
 class music_cog(commands.Cog):
     def __init__(self, bot):
+        self.list_of_denied_users = [171253608380235776, 947566204309082112]
+        self.list_of_denied_messages = ["Vous avez bien rejoint la boîte vocale de El Chapo. Veuillez laisser un message après le beep. *beep*",
+                           "Le numéro composé n'est pas en service. Veuillez raccrocher et composer de nouveau. C'était un message enregistré.",
+                           "Hello? Bye.", "You? Hmm... Maybe later.", "Password?", "Aaaanndd who are you exactly?", "https://shutupandtakemymoney.com/wp-content/uploads/2020/07/barack-obama-the-audacity-of-this-bitch-book-meme.jpg",
+                           "Go play Rocket or som'n", "Sorry, YouTube is currently out of order. You can send a support ticket at youtube.com/fuckoff",
+                           "I'm not playing this trash. K thx bye.", "Ooof, this is too trash for me.", "No.", "Ok, you are number 9347711 in queue.",
+                           "Did you ask your momma first?", "How about YOU sing?", "Hello? I can't hear you, I'm deaf.", "Sorry... I am too stupid to fulfill that request.",
+                           "Go on Spotify. Stoopid.", "Beep boop, I'm a bot.\nBeep boop, you're a cunt.", "I'm calling the police.", "I thought I was trash?",
+                           "It's a bird! No, it's a plane! No, it's a denied request.", "01001110 01101111", "If you can read this, clap your hands.", "brb",
+                           "Mmmmhh... No.", "Code it yourself.", "Beep boop, I'm a bot.\nBeep boop, suck a cock."]
         self.bot = bot
     
         #all the music related stuff
@@ -23,6 +34,7 @@ class music_cog(commands.Cog):
         self.minutesalone = 0
 
         self.client = bot
+        # Block messages from list_of_denied_users
      #searching the item on youtube
     def search_yt(self, item):
         with YoutubeDL(self.YDL_OPTIONS) as ydl:
@@ -104,6 +116,11 @@ class music_cog(commands.Cog):
 
     @commands.command(name="play", aliases=["p","playing"], help="Plays a selected song from youtube")
     async def play(self, ctx, *args):
+        print("Play command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         query = " ".join(args)
 
         # Validate input
@@ -155,6 +172,11 @@ class music_cog(commands.Cog):
 
     @commands.command(name="pause", help="Pauses the current song being played")
     async def pause(self, ctx, *args):
+        print("Pause command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         if self.is_playing:
             self.is_playing = False
             self.is_paused = True
@@ -170,6 +192,11 @@ class music_cog(commands.Cog):
 
     @commands.command(name = "resume", aliases=["r"], help="Resumes playing with the discord bot")
     async def resume(self, ctx, *args):
+        print("Resume command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         if self.is_paused:
             self.is_paused = False
             self.is_playing = True
@@ -179,6 +206,11 @@ class music_cog(commands.Cog):
 
     @commands.command(name="skip", aliases=["s"], help="Skips the current song being played")
     async def skip(self, ctx):
+        print("Skip command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         if self.is_playing:
             self.vc.stop()
             await ctx.send("Skipped the song.")
@@ -186,6 +218,11 @@ class music_cog(commands.Cog):
         
     @commands.command(name="queue", aliases=["q"], help="Displays the current songs in queue")
     async def queue(self, ctx):
+        print("Queue command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         retval = ""
         for i in range(0, len(self.music_queue)):
             # display all the songs in the queue with their index
@@ -198,7 +235,12 @@ class music_cog(commands.Cog):
             await ctx.message.add_reaction("❌")
 
     @commands.command(name="clear", aliases=["c", "bin"], help="Stops the music and clears the queue")
-    async def clear(self, ctx):     
+    async def clear(self, ctx):
+        print("Clear command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return     
         self.vc.stop()
         await ctx.send("Cleared the queue")
         await ctx.message.add_reaction("🗑️")
@@ -206,6 +248,11 @@ class music_cog(commands.Cog):
 
     @commands.command(name="leave", aliases=["disconnect", "l", "d"], help="Kick the bot from VC")
     async def dc(self, ctx):
+        print("Leave command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         self.is_playing = False
         self.is_paused = False
         await self.vc.disconnect()
@@ -214,6 +261,11 @@ class music_cog(commands.Cog):
     
     @commands.command(name="nowplaying", aliases=["np"], help="Displays the current song being played")
     async def nowplaying(self, ctx):
+        print("Nowplaying command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         if self.is_playing:
             await ctx.send("Now playing: " + self.current_song)
             await ctx.message.add_reaction("🎶")
@@ -223,6 +275,11 @@ class music_cog(commands.Cog):
 
     @commands.command(name="stop" , help="Stops the music.")
     async def stop(self, ctx):
+        print("Stop command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         if self.is_playing:
             self.vc.stop()
             await ctx.send("Stopped the song.")
@@ -234,38 +291,49 @@ class music_cog(commands.Cog):
 
     @commands.command(name="ping", help="Displays the current ping of the bot")
     async def ping(self, ctx):
+        print("Ping command triggered by: " + str(ctx.author))
+        if ctx.author.id in self.list_of_denied_users:
+            random_message = random.choice(self.list_of_denied_messages)
+            await ctx.send(random_message)
+            return
         await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
         await ctx.message.add_reaction("🏓")
 
     @commands.command(name="license", help="Displays the license of the bot")
     async def license(self, ctx):
-        await ctx.send("""
-        ```
-        MIT License
-        
-        copyright (c) 2023-2024 Ziad Lteif
+        print("License command triggered by: " + str(ctx.author))
+        if ctx.message.author.id == 127947460462116864:
+            await ctx.send("You are the owner of this bot, you already know the license.")
+            await ctx.message.add_reaction("👍")
+        else:
+            await ctx.send("""
+            ```
+            MIT License
 
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-        
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
+            copyright (c) 2023-2024 Ziad Lteif
 
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-        ```
-        """
-        )
-        await ctx.message.add_reaction("📜")
+            Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the "Software"), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:
+
+            The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.
+
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.
+            ```
+            """
+            )
+            await ctx.message.add_reaction("📜")
+        
 
     def reset(self):
         # disconnect if connected
@@ -278,4 +346,5 @@ class music_cog(commands.Cog):
         self.music_queue = []
         self.vc = None
         self.current_song = None
+    
         
